75e5d71311c9784193f820ecbf7d015332de5149
MarcosPalomo <marcos.palomo.zambrano@gmail.com>
2024-04-02 08:42:30 +0200

    Clases creadas miTienda y Producto.

---
+++ src/practicas/MiTienda.java
@@ -0,0 +1,71 @@
+package practicas;
+
+import java.util.ArrayList;
+
+/**
+ * Esta clase contiene las acciones que se pueden hacer en mi tienda.
+ *
+ * @author Marcos Palomo.
+ * @version 1.0.
+ * @since 01/04/2024.
+ * @see Producto
+ * @see TiendaMain
+ */
+
+public class MiTienda {
+
+    private ArrayList <Producto> catalogo = new ArrayList <>();
+
+    /**
+     * Constructor de la clase MiTienda.
+     */
+    public MiTienda() {
+        this.catalogo = catalogo;
+    }
+
+    /**
+     * Metodo que se usa para añadir productos a mi lista.
+     *
+     * @param producto El producto que deseo guardar en el catálogo.
+     * @return true si se agrega correctamente, false si el producto ya existe en la lista catálogo.
+     */
+
+    public boolean  agregarProducto (Producto producto){
+        if (catalogo.contains(producto)){
+            System.out.println("No se puede añadir ya que está ese producto ya está en el catalogo");
+            return false;
+        }else {
+            catalogo.add(producto);
+            return true;
+        }
+    }
+
+    /**
+     * Metodo para comprar un producto de la tienda.
+     *
+     * @param producto El producto que se quiere comprar.
+     * @return true si la compra se realiza correctamente, false si el producto no existe en la lista catálogo por lo que no se realiza la compra.
+     */
+
+    public boolean comprar (Producto producto){
+        if (catalogo.contains(producto)){
+
+            return true;
+
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * Metodo para mostrar el catalogo de productos que tiene mi tienda.
+     */
+
+    public void mostrarCatalogo(){
+
+        for (Producto producto: catalogo){
+            System.out.println(producto.toString());
+        }
+
+    }
+}
---
+++ src/practicas/Producto.java
@@ -0,0 +1,99 @@
+package practicas;
+
+import java.util.Objects;
+
+/**
+ * Clase que representa la estructura básica que tienen que tener todos los productos de mi tienda.
+ * @author Marcos Palomo.
+ * @version 1.0.
+ * @since 01/04/2024.
+ * @see MiTienda
+ *
+ */
+
+public class Producto {
+
+    private String nombre;
+    private double precio;
+
+    /**
+     * Constructor de mi clase producto.
+     *
+     * @param nombre El nombre del producto.
+     * @param precio El precio del producto.
+     */
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    /**
+     * Metodo para obtener el nombre del producto.
+     *
+     *  @return El nombre del producto.
+     */
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Metodo para obtener el precio del producto.
+     *
+     * @return El precio del producto.
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     *
+     * @param nombre El nombre nuevo.
+     * @deprecated Este metodo estña marcado como obsoleto.
+     */
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     *
+     * @param precio El precio nuevo.
+     * @deprecated Este metodo estña marcado como obsoleto.
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * Metodo para comparar si dos productos son iguales.
+     *
+     * @param o El objeto con el que deso comparar.
+     * @return true si los productos tienen el mismo nombre, false si tiene nombre distinto.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(nombre, producto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+
+    /**
+     * Método para obtener una cadena que represente un producto.
+     *
+     * @return Te devulve una cadena que representa al producto devolviendo su nombre y su precio.
+     */
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
