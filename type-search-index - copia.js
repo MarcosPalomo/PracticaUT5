085c2497e88139e40a885bcaba358323f0167da3
MarcosPalomo <marcos.palomo.zambrano@gmail.com>
2024-04-13 10:58:05 +0200

    Hola

---
+++ type-search-index - copia.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"practicas","l":"MiTienda"},{"p":"practicas","l":"Producto"},{"p":"practicas","l":"TiendaMain"}];updateSearchResults();
